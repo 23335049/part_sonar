0437223b7185eeb26f02a23c073bcc925282cd66
王雄飞 <23335049@qq.com>
2019-05-30 18:21:20 +0800

    Initial commit

---
+++ src/main/java/com/wangn/base/master.java
@@ -0,0 +1,20 @@
+package com.wangn.base;
+
+/**
+ * class functional description
+ *
+ * @author wang.xiongfei
+ * @version 1.0.0
+ * @since 2019-05-30
+ */
+public class master {
+
+    public void say() {
+        String name;
+        if (true == true) {
+            System.out.println("hehhe");
+        } else {
+            System.out.println("haha");
+        }
+    }
+}
