1d199673051c611a99414e808628dd22ee12179b
王雄飞 <23335049@qq.com>
2019-05-30 18:25:19 +0800

    add other bad file

---
+++ src/main/java/com/wangn/base/feture_1.java
@@ -0,0 +1,15 @@
+package com.wangn.base;
+
+/**
+ * class functional description
+ *
+ * @author wang.xiongfei
+ * @version 1.0.0
+ * @since 2019-05-30
+ */
+public class feture_1 {
+
+    public void say(String name) {
+        String hii = "";
+    }
+}
